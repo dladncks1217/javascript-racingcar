507294b0406969a202bdd5aa357a300bbbb20ba9
dladncks1217 <dlaxodud1217@gmail.com>
2022-12-16 16:22:22 +0900

    feat(App): 자동차 이름들 입력받기 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+const Car = require('./domain/Car');
+const InputView = require('./UI/InputView');
+const OutputView = require('./UI/OutputView');
+const makeCarList = require('./utils/makeCarList');
+const verify = require('./utils/verify');
+
 class App {
-  play() {}
+  #cars;
+  #winnerState;
+
+  play() {
+    return this.inputCarNames();
+  }
+
+  inputCarNames() {
+    InputView.inputCars((userInput) => {
+      try {
+        verify.carNameLength(userInput);
+        const cars = makeCarList(userInput);
+        return this.makeCars(cars, cars.length);
+      } catch (error) {
+        OutputView.ErrorPrintCarName();
+        return this.inputCarNames();
+      }
+    });
+  }
+
+  makeCars(cars, carCount) {
+    this.#cars = Array.from({ length: carCount }, () => 0); // hidden class 재생성 방지
+    cars.forEach((name, index) => {
+      this.#cars[index] = new Car(name);
+    });
+    console.log(this.#cars);
+  }
 }
 
 const app = new App();
